12a0493bb2f6a9e6e9ec54342a107abb9e24afb8
Tyler Brown <tyler@tmatthew.net>
2012-10-21 01:18:19 -0400

    portfolio presentation; portfolio query optimize

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 
   def index
   	@menu_item = "portfolio"
-  	@employers = Employer.where(published: true).order(:position).includes(:published_projects)
+  	@employers = Employer.where(published: true).order(:position).includes(published_projects: :screenshots)
   end
 
 end
